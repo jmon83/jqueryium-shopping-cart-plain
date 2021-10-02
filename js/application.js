f60b1266cfce9bf524b36b66b594986186719db4
jmon83 <76069447+jmon83@users.noreply.github.com>
2021-10-02 19:01:49 -0400

    REtry

--- js/application.js
+++ js/application.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   $('.qty, .price').on('keyup keypress blur change',
   function(e) {
     updateTotal();
-  });
-});
+  });});
+
 
 function updateTotal() {
   var sum = 0.0;
@@ -19,12 +19,41 @@ function updateTotal() {
   $('.total').text(sum);
 }
 
-var plusQty;
-var minusQty;
+$(document).on('click', '#addItem', function() {
+  addItem($('#name').val(), $('#cost').val());
+});
+
+var addItem = function(name,cost) {
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  $('#theStuff').prepend('<tr>\
+  <td>\
+    <div class = "product-img">\
+      <div class = "img-product">\
+        <img src = "https://picsum.photos/50">\
+      </div>\
+    </div>\
+  </td>\
+  <td>\
+    <p>' + name + '</p>\
+  </td>\
+  <td>\
+    <div class = "button-container">\
+      <button class = "cart-qty-plus" value = "+">+</button>\
+      <input type = "text" name = "qty" min = "0" class = "qty form-control" value = "0"/>\
+      <button class = "cart-qty-minus" type = "button" value = "-">-</button>\
+    </div>\
+  </td>\
+  <td>\
+    <input type = "text" value = "' + cost +'" class = "price form-control" disabled>\
+  </td>\
+  <td align = "right"> $ <span id = "amount" class = "amount"> 0 </span> </td>\
+</tr>')
+};
+
 var addBtn = $('.cart-qty-plus');
 var subBtn = $('.cart-qty-minus');
 
-var plusQty = addBtn.click(function() {
+$(document).on('click', '.cart-qty-plus', function() {
   var $n = $(this)
   .parent(".button-container")
   .find(".qty");
@@ -32,7 +61,7 @@ var plusQty = addBtn.click(function() {
   updateTotal();
 });
 
-var minusQty = subBtn.click(function() {
+$(document).on("click", '.cart-qty-minus', function() {
   var $n = $(this)
   .parent(".button-container")
   .find(".qty");
@@ -41,6 +70,4 @@ var minusQty = subBtn.click(function() {
     $n.val(qtyVal - 1);
   }
   updateTotal();
-
-
 });
